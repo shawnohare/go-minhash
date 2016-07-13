acd42555d7ec11e075421687c2de6c9b27d87b2f
Shawn O'Hare <ohare.shawn@gmail.com>
2016-07-13 10:07:48 -0700

    Move funcs.

--- iface.go
+++ iface.go
@@ -30,3 +30,38 @@ func IsEmpty(m Interface) bool {
 	}
 	return empty
 }
+
+// Similarity computes the similarity of two signatures represented
+// as MinHash instances.  This estimates the Jaccard index of the
+// two underlying sets.
+func (m *MinHash) Similarity(m2 Interface) float64 {
+	return Similarity(m, m2)
+}
+
+// Cardinality estimates the cardinality of the set. Both the
+// signature for the empty set and the zero signature have 0 cardinality.
+func (m *MinHash) Cardinality() int {
+	return Cardinality(m)
+}
+
+// UnionCardinality estimates the cardinality of the union.
+func (m *MinHash) UnionCardinality(m2 Interface) int {
+	return UnionCardinality(m, m2)
+}
+
+// IntersectionCardinality estimates the cardinality of the intersection.
+func (m *MinHash) IntersectionCardinality(m2 Interface) int {
+	return IntersectionCardinality(m, m2)
+}
+
+// SymmetricDifferenceCardinality estimates the difference between
+// the cardinality of the union and intersection.
+func (m *MinHash) SymmetricDifferenceCardinality(m2 Interface) int {
+	return SymmetricDifferenceCardinality(m, m2)
+}
+
+// LessCardinality estimates the cardinality of the left set minus
+// the right set. This operator is not symmetric.
+func (m *MinHash) LessCardinality(m2 Interface) int {
+	return LessCardinality(m, m2)
+}
